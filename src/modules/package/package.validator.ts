95b1b8fcf8f5a4f09e8d8faf0a18f8ff1c014641
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-02-21 17:55:38 +0600

    remove package escape validator

--- src/modules/package/package.validator.ts
+++ src/modules/package/package.validator.ts
@@ -1,8 +1,8 @@
 import { check, query } from "express-validator";
 
 export const addPackageValidator = [
-    check('shortTitle').trim().escape().not().isEmpty().withMessage('shortTitle is required').isString().isLength({ min: 2 }).withMessage('shortTitle min length is 2'),
-    check('shortDescription').trim().escape().not().isEmpty().withMessage('shortDescription is required').isString().isLength({ min: 2 }).withMessage('shortDescription min length is 2'),
+    check('shortTitle').trim().not().isEmpty().withMessage('shortTitle is required').isString().isLength({ min: 2 }).withMessage('shortTitle min length is 2'),
+    check('shortDescription').trim().not().isEmpty().withMessage('shortDescription is required').isString().isLength({ min: 2 }).withMessage('shortDescription min length is 2'),
     check('comment_limit').trim().escape().not().isEmpty().withMessage('comment limit is required').isNumeric().withMessage('invalid comment_limit format'),
     check('duration').trim().escape().not().isEmpty().withMessage('duration day is required').isNumeric().withMessage('invalid duration format'),
     check('plan_type').trim().escape().not().isEmpty().isString().withMessage('plan_type is required'),

cf124af3f4c54bdeaadbcee7c7f1d9fb8bfb31f3
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-03-13 18:00:12 +0600

    update muschool teachers

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -263,7 +263,7 @@ const deleteSchool_teacher = async (id: string, userId: string) => {
         );
     }
 
-    const deleted = await User.updateOne({ _id: id }, {isDeleted : true, school_admin : ''});
+    const deleted = await User.updateOne({ _id: id }, {isDeleted : true});
 
     // ----------increment add teacher-------------
     await Access_comments.updateOne({ user: userId }, { $inc: { added_member: -1 } });
